d3926decce00dd8ea1d2f76be3528fadef71b18b
StevenCurran <scurran14@qub.ac.uk>
2013-10-28 22:23:59 +0000

    Train with a different data set

--- tests/sae_classifier.m
+++ tests/sae_classifier.m
@@ -11,16 +11,17 @@ test_y  = double(test_y);
 s = loadAllDigitsIntoStruct(test_x, test_y);
 test = vertcat(s.f0, s.f8);
 
-training = datasample(test,784);
+%training = datasample(test,784);
 rand('state',0)
-sae = saesetup([784 100]);
+%sae = saesetup([784 100]);
+sae = saesetup([length(test) 100]);
 sae.ae{1}.activation_function       = 'sigm';
 sae.ae{1}.learningRate              = 1;
 sae.ae{1}.inputZeroMaskedFraction   = 0.5;
 opts.numepochs =   25;
 opts.batchsize = 2;
 %sae = saetrain(sae, train_x, opts);
-sae = saetrain(sae, training, opts);
+sae = saetrain(sae, test, opts);
 visualize(sae.ae{1}.W{1}(:,2:end)')
 
 

e83c5853882bfd3b0c7efc8757eb027479eac53e
StevenCurran <scurran14@qub.ac.uk>
2013-11-21 03:15:28 +0000

    add in the libsvm lbirary

--- tests/createLabelVector.m
+++ tests/createLabelVector.m
@@ -3,7 +3,7 @@ function [labelVec] = createLabelVector(vectors, labels)
 %   Takes 2 vectors as inputs and creates a 1D vector to output the
 %   relevent labels based on the size of the vectors that has been fed to
 %   the function.
-labelVec = []
+labelVec = [];
 for i=1 : numel(vectors)
     for j=1 : vectors(i)
         %labelVec = [labelVec labels(i)];
@@ -18,8 +18,24 @@ end
 function [labelVector] = genLabelVector(label)
     switch(label)
         case '0'
-            labelVector = [0] % [0 1]
+            labelVector = [0]; % [0 1]
+        case '1'
+            labelVector = [1];
+        case '2'
+            labelVector = [2];
+        case '3'
+            labelVector = [3];
+        case '4'
+            labelVector = [4];
+        case '5'
+            labelVector = [5];
+        case '6'
+            labelVector = [6];            
+        case '7'
+            labelVector = [7];
+        case '9'
+            labelVector = [9];            
         case '8'
-            labelVector = [1]
+            labelVector = [8];
     end
 end
--- tests/sae_classifier.m
+++ tests/sae_classifier.m
@@ -14,8 +14,8 @@ s = loadAllDigitsIntoStruct(test_x, test_y);
 imageToClassify = s.f8(1,:);
 s.f8 = s.f8(3:end,:);
 
-test = vertcat(s.f0, s.f8);
-labels = createLabelVector([length(s.f0) length(s.f8)],['0' '8']);
+test = vertcat(s.f0, s.f8,s.f1);
+labels = createLabelVector([length(s.f0) length(s.f8) length(s.f1)],['0' '8' '1']);
 
 %training = datasample(test,784);
 rand('state',0)
@@ -25,7 +25,7 @@ sae.ae{1}.activation_function       = 'sigm';
 sae.ae{1}.learningRate              = 0.1;
 sae.ae{1}.inputZeroMaskedFraction   = 0.5;
 opts.numepochs =   5;
-opts.batchsize = 2;
+opts.batchsize = 1;
 %sae = saetrain(sae, train_x, opts);
 sae = saetrain(sae, test, opts);
 visualize(sae.ae{1}.W{1}(:,2:end)')
@@ -36,9 +36,13 @@ visualize(sae.ae{1}.W{1}(:,2:end)')
 %activations = sigm(input * nn.W{i - 1}');
 %activations = sigm(input * sae.ae{1}.W{1}(:,2:end)'); % should be of size 1954
 activations1 = sigm(test * sae.ae{1}.W{2}(:,2:end));
+
 %[size of test , size of hidden]
 %pass to svmtrain with labels
-SVMStruct = svmtrain(activations1,labels);
+%SVMStruct = svmtrain(activations1,labels,'showplot',true);
+%try with lib svm
+SVMStruct = svmtrain(activations1,labels,[]);
+
 
 %Load in a new image
 
@@ -48,6 +52,8 @@ activations2 = sigm(imageToClassify * sae.ae{1}.W{2}(:,2:end));
 %[1 , size of hidden]
 %pass to svmtest
 %return the label
+
+predicted_label = svmpredict(labels, activations2, SVMStruct, []);
 
 digit = svmclassify(SVMStruct,activations2,'showplot',true)
 hold on;

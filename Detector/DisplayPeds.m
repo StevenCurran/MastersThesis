35d01e203d02ffc1cf48406251a8260d8ea04899
StevenCurran <scurran14@qub.ac.uk>
2014-03-12 19:17:20 +0000

    ada

--- Detector/DisplayPeds.m
+++ Detector/DisplayPeds.m
@@ -1,4 +1,4 @@
-function [] = DisplayPeds( cnn, frameNumber, boxH, boxW)
+function [] = DisplayPeds()
 
 frameNumber = 10;
 boxH = 52;
@@ -6,27 +6,33 @@ boxW = 32;
 
 movie = VideoReader('TownCentreXVID.avi');
 frame = read(movie, frameNumber);
- frame = rgb2gray(frame);
- 
- framesMap = containers.Map;
+frame = rgb2gray(frame);
+
+framesMap = containers.Map;
 
 %get all the frames from the image...
-for i = 1 : 50 :(size(frame, 1) - boxH)
-    for j = 1 : 50 :(size(frame, 2)-boxW)
-        image = frame(i:i+boxH-1, j:j+boxW-1);
-        image = double(image)/255;
-        
-        %zcaNonPeople = reshape(image, boxH*boxW, 1);
-        %whitenNonPeople = zcaWhiten(zcaNonPeople);
-        %image = reshape(whitenNonPeople, boxH,boxW,1);
-        
-        framesMap([num2str(i) ':' num2str(j)]) = image;
+for h = 1 :-0.2:0.2
+    scaledFrame = imresize(frame, h);
+    for i = 1 : 15 :(size(scaledFrame, 1) - boxH)
+        for j = 1 : 15 :(size(scaledFrame , 2)-boxW)
+            image = scaledFrame(i:i+boxH-1, j:j+boxW-1);
+            image = double(image)/255;
+            framesMap([num2str(h) ':' num2str(i) ':' num2str(j)]) = image;
+        end
     end
 end
 
 testImages = ConvertFromCellArray(framesMap.values);
 
-net = cnnff(cnn, testImages);
+zcaPeople = reshape(testImages, boxH*boxW, length(testImages));
+whitenPeople = zcaWhiten(zcaPeople);
+whitenPeople = reshape(whitenPeople, boxH,boxW,length(testImages));
+
+
+keys = framesMap.keys();
+xy = strsplit(keys{100}, ':')
+
+net = cnnff(cnn, whitenPeople);
 
 
 

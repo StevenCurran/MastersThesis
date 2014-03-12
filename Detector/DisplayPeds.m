3e451d6b2817c51f41f1b8794358db627a10d439
StevenCurran <scurran14@qub.ac.uk>
2014-03-12 18:07:10 +0000

    ped detector

--- Detector/DisplayPeds.m
+++ Detector/DisplayPeds.m
@@ -6,17 +6,31 @@ boxW = 32;
 
 movie = VideoReader('TownCentreXVID.avi');
 frame = read(movie, frameNumber);
-frame = frame(:,:,1);
+ frame = rgb2gray(frame);
+ 
+ framesMap = containers.Map;
 
-for i = 1 : 5 :size(frame, 1)
-    for j = 1 : 5 :size(frame, 2)
+%get all the frames from the image...
+for i = 1 : 50 :(size(frame, 1) - boxH)
+    for j = 1 : 50 :(size(frame, 2)-boxW)
+        image = frame(i:i+boxH-1, j:j+boxW-1);
+        image = double(image)/255;
         
+        %zcaNonPeople = reshape(image, boxH*boxW, 1);
+        %whitenNonPeople = zcaWhiten(zcaNonPeople);
+        %image = reshape(whitenNonPeople, boxH,boxW,1);
+        
+        framesMap([num2str(i) ':' num2str(j)]) = image;
     end
 end
 
+testImages = ConvertFromCellArray(framesMap.values);
+
+net = cnnff(cnn, testImages);
 
 
-imshow(frame);
+
+
 
 
 

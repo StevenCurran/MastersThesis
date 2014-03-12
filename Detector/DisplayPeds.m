2aacb73b64612c79e8916bf899b6ef0cc006ef00
StevenCurran <scurran14@qub.ac.uk>
2014-03-12 23:52:58 +0000

    make training data smaller

--- Detector/DisplayPeds.m
+++ Detector/DisplayPeds.m
@@ -1,6 +1,6 @@
 function [] = DisplayPeds()
 
-frameNumber = 10;
+frameNumber = 20;
 boxH = 150;
 boxW = 80;
 
@@ -14,14 +14,11 @@ framesMap = containers.Map;
 %get all the frames from the image...
 for h = 1 : 1
     scaledFrame = imresize(frame, h);
-    for i = 1 : 15 :(size(scaledFrame, 1) - boxH)
-        for j = 1 : 15 :(size(scaledFrame , 2)-boxW)
+    for i = 1 : 5 :(size(scaledFrame, 1) - boxH)
+        for j = 1 : 5 :(size(scaledFrame , 2)-boxW)
             image = scaledFrame(i:i+boxH-1, j:j+boxW-1);
             image = double(image)/255;
-            if(i>900)
-                disp(i);
-            end
-            
+            image = imresize(image, [52 32]);
             framesMap([num2str(h) ':' num2str(i) ':' num2str(j)]) = image;
         end
     end
@@ -29,31 +26,37 @@ end
 
 testImages = ConvertFromCellArray(framesMap.values);
 
-zcaPeople = reshape(testImages, boxH*boxW, length(testImages));
-whitenPeople = zcaWhiten(zcaPeople);
-whitenPeople = reshape(whitenPeople, boxH,boxW,length(testImages));
+%testImages = imresize(testImages, [52 32]);
 
+% zcaPeople = reshape(testImages, boxH*boxW, length(testImages));
+% whitenPeople = zcaWhiten(zcaPeople);
+% whitenPeople = reshape(whitenPeople, boxH,boxW,length(testImages));
+
+zcaPeople = reshape(testImages, 52*32, length(testImages));
+whitenPeople = zcaWhiten(zcaPeople);
+whitenPeople = reshape(whitenPeople, 52,32,length(testImages));
 
 keys = framesMap.keys();
 
-smallerPeople = imresize(whitenPeople, [52 32]);
+keys = keys(1:5:end);
+whitenPeople = whitenPeople(:,:,1:5:end);
 
-net = cnnff(cnn, smallerPeople);
+net = cnnff(cnn, whitenPeople);
 
-estimate = find(net.o(2,:) > 0.9);
+estimate = find(net.o(2,:) > 0.95);
 
 figure;
 imshow(colorFrame);
 axis on;
 hold on;
 
-for es = 1 : length(keys)
-    xy = strsplit(keys{es}, ':');
-    x = str2double(xy{2});
-    y = str2double(xy{3});
-   plot(y,x,'r');
-    
-end
+% for es = 1 : length(keys)
+%     xy = strsplit(keys{es}, ':');
+%     x = str2double(xy{2});
+%     y = str2double(xy{3});
+%    plot(y,x,'r');
+%     
+% end
 
 
 for es = 1 : length(estimate)

ff50bf13b4ca38ff4590f07268294ca4a2a1a4e2
StevenCurran <scurran14@qub.ac.uk>
2014-04-14 13:54:53 +0100

    changes to detection code

--- Detector/validateDetections.m
+++ Detector/validateDetections.m
@@ -1,13 +1,12 @@
-function [ output_args ] = validateDetections( frameNumber, detectorOutput, width, height )
+function [precision,recall] = validateDetections( frameNumber, detectorOutput, width, height )
 
 
 % frame trackNumber boundingBoxPosition_y boundingBoxPosition_x boundingBoxWidth boundingBoxHeight
 decOut = [];
 for i = 1 : length(detectorOutput)
-    y = finalBoxes(es, 1); 
-    x = finalBoxes(es, 2); 
-    decOut = vertcat(decOut, [x y width height]);
-    
+    y = detectorOutput(i, 1); 
+    x = detectorOutput(i, 2); 
+    decOut = vertcat(decOut, [frameNumber 0 y x width height]);
 end
 
 
@@ -15,8 +14,12 @@ end
 produceVideo = false;
 videoFilename = 'TownCentreXVID.avi';
 
+groundTruthFile = 'TownCentreGTHalfRes.csv';
+verbose = true;    
+isOxford = false; 
 
 
+[mota,motp,precision,recall] = measureMOTAOxford(groundTruthFile,decOut,verbose,isOxford,produceVideo,videoFilename);
 
 end
 

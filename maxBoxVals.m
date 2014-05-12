f3cd67230fa149cc711fda9006c27371bc5d1d92
StevenCurran <scurran14@qub.ac.uk>
2014-05-13 00:16:26 +0100

    box optimisation

---
+++ maxBoxVals.m
@@ -0,0 +1,50 @@
+function localBoxes = maxBoxVals(finalBoxes)
+
+rects = [];
+for es = 1 : length(finalBoxes)
+    y = finalBoxes(es, 1); 
+    x = finalBoxes(es, 2); 
+    y2 = finalBoxes(es, 3);
+    x2 = finalBoxes(es, 4); 
+    rects = [rects ; [x, y, y2-y, x2 - x]];
+end
+
+overlaps = rectint(rects, rects);
+
+%overlaps(logical(eye(size(overlaps)))) = 0;
+
+[r,c] = find(overlaps > 0);
+localBoxes = [];
+
+for i = 1 : length(r)
+    
+    
+end
+
+for i = 1 : length(overlaps)
+    col = overlaps(:,i);
+    col2 = find(col > 0);
+    
+    if(~length(col2) > 0)
+        continue;
+    end
+    localRects = [rects(i,:)];
+    for j = 1 : length(col2)
+        localRects = [localRects ; rects(col2(j),:)];
+    end
+    
+    if(max(localRects) > 0)
+        minX = min(localRects(:,1));
+        minY = min(localRects(:,2));
+        maxX = max(localRects(:,1))+max(localRects(:,3));
+        maxY = max(localRects(:,2))+max(localRects(:,4));
+        localBoxes = [localBoxes ; [minX,minY,abs(maxX - minX), abs(maxY - minY)]];
+    end
+    
+end
+
+localBoxes = unique(localBoxes,'rows')
+
+
+
+end
